a664010220c5b62dc3394ab9af3c4d3f97243684
Alice Creasey <alice.creasey@hotmail.com>
2018-05-05 09:57:56 +0100

    Set up sliders
    Currently they spit a value back into the page - need to take this value and feed into the logic.

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
     <title>Sky | Moodslider</title>
   </head>
   <body>
-      <nav class="navbar navbar-expand-sm fixed-top navbar-light">
-          <div class="container">
-          <a class="navbar-brand" href="#"><img src="images/sky-logo.jpg" alt="Sky logo" style="width: 80px;"/></a>
+      <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="container">
+        <a class="navbar-brand" href="#"><img src="images/sky-logo.jpg" alt="Sky logo" style="width: 80px;"/></a>
             <!-- Toggler/collapsibe Button -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="#">Watch</a>
@@ -38,9 +38,86 @@
                     <a class="nav-link" href="#">Upload</a>
                 </li>
             </ul>
-          </div>
-          </div>
+        </div>
+        </div>
       </nav>
+      
+      <section class="container-fluid">
+          <div class="container" style="text-align: center;">
+            <h1>Moodslider</h1>
+            <h3>Not sure what to watch?</h3>
+            <p>Move the sliders to reflect your mood and we'll suggest the perfect programme.</p>
+          </div>
+      </section>
+      
+      <section class="container-fluid" style="background-color: #F1F1F3;">
+          <div class="container" style="text-align: center;">
+              
+              <div class="row" style="padding-top: 20px; padding-bottom: 20px;">
+                  <div class="col-sm-3">
+                      <h3>Happy</h3>
+                  </div>
+                  
+                  <div class="col-sm-6">
+                      <div class="slidecontainer">
+                        <input type="range" min="1" max="5" value="3" class="slider" id="happySad">
+                        <p>Chosen: <span id="happySadValue"></span></p>
+                      </div>
+                  </div>
+                  
+                  <div class="col-sm-3">
+                      <h3>Sad</h3>
+                  </div>
+              </div>
+              
+              
+              <div class="row" style="padding-top: 20px; padding-bottom: 20px;">
+                  <div class="col-sm-3">
+                      <h3>Calm</h3>
+                  </div>
+                  <div class="col-sm-6">
+                      <div class="slidecontainer">
+                        <input type="range" min="1" max="5" value="3" class="slider" id="calmAgi">
+                        <p>Chosen: <span id="calmAgiValue"></span></p>
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <h3>Agitated</h3>
+                  </div>
+              </div>
+              
+              <div class="row" style="padding-top: 20px; padding-bottom: 20px;">
+                  <div class="col-sm-3">
+                      <h3>Tired</h3>
+                  </div>
+                  <div class="col-sm-6">
+                      <div class="slidecontainer">
+                        <input type="range" min="1" max="5" value="3" class="slider" id="tiredAwake">
+                        <p>Chosen: <span id="tiredAwakeValue"></span></p>
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <h3>Wide awake</h3>
+                  </div>
+              </div>
+              
+              <div class="row" style="padding-top: 20px; padding-bottom: 20px;">
+                  <div class="col-sm-3">
+                      <h3>Brave</h3>
+                  </div>
+                  <div class="col-sm-6">
+                      <div class="slidecontainer">
+                        <input type="range" min="1" max="5" value="3" class="slider" id="braveScared">
+                        <p>Chosen: <span id="braveScaredValue"></span></p>
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <h3>Scared</h3>
+                  </div>
+              </div>
+          </div>
+      </section>
+      
           
       
       
@@ -48,9 +125,41 @@
       
       
       
+    <script>
+    var slider1 = document.getElementById("happySad");
+    var output1 = document.getElementById("happySadValue");
+    output1.innerHTML = slider1.value;
+
+    slider1.oninput = function() {
+      output1.innerHTML = this.value;
+    };
+    
+    var slider2 = document.getElementById("calmAgi");
+    var output2 = document.getElementById("calmAgiValue");
+    output2.innerHTML = slider2.value;
+
+    slider2.oninput = function() {
+      output2.innerHTML = this.value;
+    };
+
+    var slider3 = document.getElementById("tiredAwake");
+    var output3 = document.getElementById("tiredAwakeValue");
+    output3.innerHTML = slider3.value;
+
+    slider3.oninput = function() {
+      output3.innerHTML = this.value;
+    };
+
+    var slider4 = document.getElementById("braveScared");
+    var output4 = document.getElementById("braveScaredValue");
+    output4.innerHTML = slider4.value;
+
+    slider4.oninput = function() {
+      output4.innerHTML = this.value;
+    };
+    </script>      
       
-      
-      
+  
       
       
       
@@ -59,5 +168,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    
   </body>
 </html>
