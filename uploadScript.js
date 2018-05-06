892574fe2f86eab7c7187be6f7915250c7ca0f19
Alice Creasey <alice.creasey@hotmail.com>
2018-05-06 15:41:48 +0100

    removing the old js
    not needed any more

--- uploadScript.js
+++
@@ -1,9 +0,0 @@
-   
-   function showInfo() {
-    var info = document.getElementById("moreInfo");
-    if (info.style.display === "none") {
-        info.style.display = "block";
-    } else {
-        info.style.display = "none";
-    }
-}
