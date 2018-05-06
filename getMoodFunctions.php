d69c176f4866ce846bc6a9c017c887405a507470
Alice Creasey <alice.creasey@hotmail.com>
2018-05-06 09:54:10 +0100

    Tidy up getMoodFunctions

--- getMoodFunctions.php
+++ getMoodFunctions.php
@@ -1,64 +1,73 @@
 <?php
 
-function getMood($mood, $moodValue)   {
-    if ($mood == "happy" && $moodValue == 3){
-        return "You're feeling okay";
-    }
-    else if ($mood == "happy" && $moodValue == 2){
-        return "You are quite happy";
-    }
-    else if ($mood == "happy" && $moodValue == 1){
+function howHappy($moodValue)    {
+    if ($moodValue == 1){
         return "You are very happy!";
     }
-    else if ($mood == "happy" && $moodValue == 4){
+    else if ($moodValue == 2){
+        return "You are quite happy";
+    }
+    else if ($moodValue == 3){
+        return "You're feeling okay";
+    }
+    else if ($moodValue == 4){
         return "You are a bit sad";
     }
-    else if ($mood == "happy" && $moodValue == 5){
+    else if ($moodValue == 5){
         return "You are really sad";
     }
-    else if ($mood == "calm" && $moodValue == 1){
+}
+
+function howCalm($moodValue) {
+    if ($moodValue == 1){
         return "Feeling zen";
     }
-    else if ($mood == "calm" && $moodValue == 2){
+    else if ($moodValue == 2){
         return "Feeling fairly chill";
     }
-    else if ($mood == "calm" && $moodValue == 3){
+    else if ($moodValue == 3){
         return "You are middling";
     }
-    else if ($mood == "calm" && $moodValue == 4){
+    else if ($moodValue == 4){
         return "Feeling a bit stressed";
     }
-    else if ($mood == "calm" && $moodValue == 5){
+    else if ($moodValue == 5){
         return "Let's find something to calm you down";
     }
-    else if ($mood == "tired" && $moodValue == 1){
+}
+
+function howTired($moodValue)    {
+    if ($moodValue == 1){
         return "Try to stay awake for this one";
     }
-    else if ($mood == "tired" && $moodValue == 2){
+    else if ($moodValue == 2){
         return "You're pretty sleepy";
     }
-    else if ($mood == "tired" && $moodValue == 3){
+    else if ($moodValue == 3){
         return "Feeling okay";
     }
-    else if ($mood == "tired" && $moodValue == 4){
+    else if ($moodValue == 4){
         return "Let's find something exciting";
     }
-    else if ($mood == "tired" && $moodValue == 5){
+    else if ($moodValue == 5){
         return "You've got to concentrate for these ones";
     }
-    else if ($mood == "brave" && $moodValue == 1){
+}
+
+function howBrave($moodValue)    {
+    if ($moodValue == 1){
         return "Let's find something on the scary side...";
     }
-    else if ($mood == "brave" && $moodValue == 2){
+    else if ($moodValue == 2){
         return "Here are some onptions to keep you on your toes";
     }
-    else if ($mood == "brave" && $moodValue == 3){
+    else if ($moodValue == 3){
         return "Nothing too scary then";
     }
-    else if ($mood == "brave" && $moodValue == 4){
+    else if ($moodValue == 4){
         return "You're feeling a little bit scared";
     }
-    else if ($mood == "brave" && $moodValue == 5){
+    else if ($moodValue == 5){
         return "Here are some options that won't make you jump";
     }
 }
@@ -66,9 +75,21 @@ function getMood($mood, $moodValue)   {
 $mood = $_REQUEST["mood"];
 $moodValue = $_REQUEST["moodValue"];
 
-$moodDescription = getMood($mood, $moodValue);
+if ($mood == "happy")   {
+    $moodDescription = howHappy($moodValue);
+}
+else if ($mood == "calm")   {
+    $moodDescription = howCalm($moodValue);
+}
+else if ($mood == "tired")   {
+    $moodDescription = howTired($moodValue);
+}
+else if ($mood == "brave")   {
+    $moodDescription = howBrave($moodValue);
+}
 
 echo $moodDescription;
+
 
 
 //function readXMLFile()  {
--- index.php
+++ index.php
@@ -172,67 +172,70 @@
       
       
     <script>
-        var happySlider = document.getElementById("happy");
+//    Happy slider    
+    var happySlider = document.getElementById("happy");
         
-        happySlider.oninput = function () {
-            happyValue = this.value;
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    document.getElementById("happyValue").innerHTML = this.responseText;
-                }
+    happySlider.oninput = function () {
+        happyValue = this.value;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById("happyValue").innerHTML = this.responseText;
             }
-            
-            xmlhttp.open("Get", "getMoodFunctions.php?mood=happy&moodValue=" + happyValue, true);
-            xmlhttp.send();
-        };
-        
-        var calmSlider = document.getElementById("calm");
-        
-        calmSlider.oninput = function () {
-            calmValue = this.value;
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    document.getElementById("calmValue").innerHTML = this.responseText;
-                }
-            }
-            
-            xmlhttp.open("Get", "getMoodFunctions.php?mood=calm&moodValue=" + calmValue, true);
-            xmlhttp.send();
-        };
-        
-        var tiredSlider = document.getElementById("tired");
-        
-        tiredSlider.oninput = function () {
-            tiredValue = this.value;
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    document.getElementById("tiredValue").innerHTML = this.responseText;
-                }
-            }
-            
-            xmlhttp.open("Get", "getMoodFunctions.php?mood=tired&moodValue=" + tiredValue, true);
-            xmlhttp.send();
-        };
-        
-        var braveSlider = document.getElementById("brave");
-        
-        braveSlider.oninput = function () {
-            braveValue = this.value;
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    document.getElementById("braveValue").innerHTML = this.responseText;
-                }
-            }
-            
-            xmlhttp.open("Get", "getMoodFunctions.php?mood=brave&moodValue=" + braveValue, true);
-            xmlhttp.send();
-        };
-     
+        }
+
+        xmlhttp.open("Get", "getMoodFunctions.php?mood=happy&moodValue=" + happyValue, true);
+        xmlhttp.send();
+    };
        
+//    Calm slider
+    var calmSlider = document.getElementById("calm");
+
+    calmSlider.oninput = function () {
+        calmValue = this.value;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById("calmValue").innerHTML = this.responseText;
+            }
+        }
+
+        xmlhttp.open("Get", "getMoodFunctions.php?mood=calm&moodValue=" + calmValue, true);
+        xmlhttp.send();
+    };
+        
+//    Tired slider
+    var tiredSlider = document.getElementById("tired");
+
+    tiredSlider.oninput = function () {
+        tiredValue = this.value;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById("tiredValue").innerHTML = this.responseText;
+            }
+        }
+
+        xmlhttp.open("Get", "getMoodFunctions.php?mood=tired&moodValue=" + tiredValue, true);
+        xmlhttp.send();
+    };
+        
+//    Brave slider
+    var braveSlider = document.getElementById("brave");
+
+    braveSlider.oninput = function () {
+        braveValue = this.value;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                document.getElementById("braveValue").innerHTML = this.responseText;
+            }
+        }
+
+        xmlhttp.open("Get", "getMoodFunctions.php?mood=brave&moodValue=" + braveValue, true);
+        xmlhttp.send();
+    };
+     
     </script>      
       
   
